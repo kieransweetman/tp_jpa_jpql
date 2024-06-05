62dff7def876483954a9e3cb16906e4849a1d00f
kieransweetman <kikisweetman@gmail.com>
2024-06-05 15:00:20 +0200

    completed all tests

--- src/main/java/fr/diginamic/Realisateur.java
+++ src/main/java/fr/diginamic/Realisateur.java
@@ -13,100 +13,129 @@ import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 @Entity
-@Table(name="REALISATEUR")
+@Table(name = "REALISATEUR")
 public class Realisateur {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name="id")
+	@Column(name = "id")
 	private Long id;
-	
-	@Column(name="id_imdb")
+
+	@Column(name = "id_imdb")
 	private String idImdb;
 	private String identite;
 	private String href;
 	private LocalDate anniversaire;
-	
+
 	public Realisateur() {
-		
+
 	}
-	
+
 	public Realisateur(String idImdb, String identite, String href) {
 		super();
 		this.idImdb = idImdb;
 		this.identite = identite;
 		this.href = href;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		return new HashCodeBuilder().append(idImdb).toHashCode();
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		if (!(obj instanceof Realisateur)) {
 			return false;
 		}
-		Realisateur autre = (Realisateur)obj;
+		Realisateur autre = (Realisateur) obj;
 		return new EqualsBuilder().append(idImdb, autre.getIdImdb()).isEquals();
 	}
-	
-	/** Getter
+
+	/**
+	 * Getter
+	 * 
 	 * @return the id
 	 */
 	public Long getId() {
 		return id;
 	}
-	/** Setter
+
+	/**
+	 * Setter
+	 * 
 	 * @param id the id to set
 	 */
 	public void setId(Long id) {
 		this.id = id;
 	}
-	/** Getter
+
+	/**
+	 * Getter
+	 * 
 	 * @return the idImdb
 	 */
 	public String getIdImdb() {
 		return idImdb;
 	}
-	/** Setter
+
+	/**
+	 * Setter
+	 * 
 	 * @param idImdb the idImdb to set
 	 */
 	public void setIdImdb(String idImdb) {
 		this.idImdb = idImdb;
 	}
-	/** Getter
+
+	/**
+	 * Getter
+	 * 
 	 * @return the identite
 	 */
 	public String getIdentite() {
 		return identite;
 	}
-	/** Setter
+
+	/**
+	 * Setter
+	 * 
 	 * @param identite the identite to set
 	 */
 	public void setIdentite(String identite) {
 		this.identite = identite;
 	}
-	/** Getter
+
+	/**
+	 * Getter
+	 * 
 	 * @return the href
 	 */
 	public String getHref() {
 		return href;
 	}
-	/** Setter
+
+	/**
+	 * Setter
+	 * 
 	 * @param href the href to set
 	 */
 	public void setHref(String href) {
 		this.href = href;
 	}
-	/** Getter
+
+	/**
+	 * Getter
+	 * 
 	 * @return the anniversaire
 	 */
 	public LocalDate getAnniversaire() {
 		return anniversaire;
 	}
-	/** Setter
+
+	/**
+	 * Setter
+	 * 
 	 * @param anniversaire the anniversaire to set
 	 */
 	public void setAnniversaire(LocalDate anniversaire) {
--- src/test/java/fr/diginamic/ActeurRepositoryTest.java
+++ src/test/java/fr/diginamic/ActeurRepositoryTest.java
@@ -12,102 +12,132 @@ import javax.persistence.TypedQuery;
 import org.junit.Test;
 
 public class ActeurRepositoryTest {
-	
-	private EntityManagerFactory emf = Persistence.createEntityManagerFactory("movie_db");
+
+	private EntityManagerFactory emf = Persistence.createEntityManagerFactory("movies");
 	private EntityManager em = emf.createEntityManager();
-	
+
 	/**
 	 * Extraire tous les acteurs triés dans l'ordre alphabétique des identités
 	 */
 	@Test
 	public void testExtraireActeursTriesParIdentite() {
-		
-		TypedQuery<Acteur> query = em.createQuery("SELECT a FROM Acteur a", Acteur.class);
+
+		TypedQuery<Acteur> query = em.createQuery("SELECT a FROM Acteur a order by a.identite asc", Acteur.class);
 		List<Acteur> acteurs = query.getResultList();
-		
+
 		assertEquals(1137, acteurs.size());
 		assertEquals("A.J. Danna", acteurs.get(0).getIdentite());
 	}
-	
+
 	/**
 	 * Extraire l'actrice appelée Marion Cotillard
 	 */
 	@Test
 	public void testExtraireActeursParIdentite() {
-		TypedQuery<Acteur> query = em.createQuery("SELECT a FROM Acteur a", Acteur.class);
+		TypedQuery<Acteur> query = em.createQuery("SELECT a FROM Acteur a where a.identite = 'Marion Cotillard'",
+				Acteur.class);
 		List<Acteur> acteurs = query.getResultList();
-		
+
 		assertEquals(1, acteurs.size());
 		assertEquals("Marion Cotillard", acteurs.get(0).getIdentite());
 	}
-	
+
 	/**
 	 * Extraire la liste des acteurs dont l'année de naissance est 1985.
 	 * Astuce: fonction year(...)
 	 */
 	@Test
 	public void testExtraireActeursParAnneeNaissance() {
-		TypedQuery<Acteur> query = em.createQuery("SELECT a FROM Acteur a", Acteur.class);
+		TypedQuery<Acteur> query = em.createQuery("SELECT a FROM Acteur a where YEAR(a.anniversaire) = 1985 ",
+				Acteur.class);
 		List<Acteur> acteurs = query.getResultList();
-		
+
 		assertEquals(10, acteurs.size());
 	}
-	
+
 	/**
 	 * Extraire la liste des actrices ayant joué le rôle d'Harley QUINN
 	 */
 	@Test
 	public void testExtraireActeursParRole() {
-		
-		TypedQuery<Acteur> query = em.createQuery("SELECT a FROM Acteur a", Acteur.class);
+
+		TypedQuery<Acteur> query = em.createQuery("SELECT a FROM Acteur a JOIN a.roles r WHERE r.nom = 'Harley Quinn'",
+				Acteur.class);
 		List<Acteur> acteurs = query.getResultList();
-		
+
 		assertEquals(2, acteurs.size());
 		assertEquals("Margot Robbie", acteurs.get(0).getIdentite());
 		assertEquals("Margot Robbie", acteurs.get(1).getIdentite());
 	}
-	
+
 	/**
 	 * Extraire la liste de tous les acteurs ayant joué dans un film paru en 2015.
 	 */
 	@Test
 	public void testExtraireActeursParFilmParuAnnee() {
-		TypedQuery<Acteur> query = em.createQuery("SELECT a FROM Acteur a", Acteur.class);
+		TypedQuery<Acteur> query = em.createQuery(
+				"SELECT a FROM Acteur a " +
+						"LEFT JOIN a.roles r " +
+						"LEFT JOIN r.film f " +
+						"where f.annee = 2015",
+				Acteur.class);
 		List<Acteur> acteurs = query.getResultList();
 		assertEquals(140, acteurs.size());
 	}
-	
+
 	/**
 	 * Extraire la liste de tous les acteurs ayant joué dans un film français
 	 * Astuce: mot clé distinct
 	 */
 	@Test
 	public void testExtraireActeursParPays() {
-		TypedQuery<Acteur> query = em.createQuery("SELECT a FROM Acteur a", Acteur.class);
+		TypedQuery<Acteur> query = em.createQuery(
+				"SELECT DISTINCT a FROM Acteur a " +
+						"JOIN a.roles r " +
+						"JOIN r.film f " +
+						"JOIN f.pays p " +
+						"WHERE p.nom = 'France'",
+				Acteur.class);
 		List<Acteur> acteurs = query.getResultList();
 		assertEquals(158, acteurs.size());
 	}
-	
+
 	/**
-	 * Extraire la liste de tous les acteurs ayant joué dans un film français paru en 2017
+	 * Extraire la liste de tous les acteurs ayant joué dans un film français paru
+	 * en 2017
 	 * Astuce: mot clé distinct
 	 */
 	@Test
 	public void testExtraireActeursParListePaysEtAnnee() {
-		TypedQuery<Acteur> query = em.createQuery("SELECT a FROM Acteur a", Acteur.class);
+		TypedQuery<Acteur> query = em.createQuery(
+				"SELECT DISTINCT a FROM Acteur a " +
+						"JOIN a.roles r " +
+						"JOIN r.film f " +
+						"JOIN f.pays p " +
+						"WHERE p.nom = 'France'" +
+						"AND f.annee = 2017 ",
+				Acteur.class);
 		List<Acteur> acteurs = query.getResultList();
 		assertEquals(24, acteurs.size());
 	}
-	
+
 	/**
-	 * Extraire la liste de tous les acteurs ayant joué dans un film réalisé par Ridley Scott 
+	 * Extraire la liste de tous les acteurs ayant joué dans un film réalisé par
+	 * Ridley Scott
 	 * entre les années 2010 et 2020
 	 * Astuce: mot clé distinct
 	 */
 	@Test
 	public void testExtraireParRealisateurEntreAnnee() {
-		TypedQuery<Acteur> query = em.createQuery("SELECT a FROM Acteur a", Acteur.class);
+		TypedQuery<Acteur> query = em.createQuery(
+				"SELECT DISTINCT a FROM Acteur a " +
+						"JOIN a.roles r " +
+						"JOIN r.film f " +
+						"JOIN f.realisateurs rr " +
+						"WHERE rr.identite = 'Ridley Scott' AND f.annee BETWEEN 2010 AND 2020",
+				Acteur.class);
 		List<Acteur> acteurs = query.getResultList();
 		assertEquals(27, acteurs.size());
 	}
+
 }
